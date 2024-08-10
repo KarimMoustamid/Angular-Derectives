a253a754ff9c141307c69b75703cd76e7e8be6da
karimmoustamid <moustamid.teck@gmail.com>
2024-08-10 10:32:26 -0600

    cAdd pagination and Bootstrap for image gallery
    Implemented a pagination component in `app.component.html` to navigate through images. Integrated Bootstrap for styling and added relevant dependencies in `package.json` and `package-lock.json`. Updated `app.component.ts` to manage pagination logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+    images = [
+        {
+            title: 'Image1',
+            url: 'https://images.unsplash.com/photo-1722260613137-f8f5ac432d69?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        },
+        {
+            title: 'Image2',
+            url: 'https://images.unsplash.com/photo-1721332155433-3a4b5446bcd9?q=80&w=3435&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        },
+        {
+            title: 'Image3',
+            url: 'https://images.unsplash.com/photo-1723101174160-52a0adb173ff?q=80&w=2160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        },
+        {
+            title: 'Image4',
+            url: 'https://images.unsplash.com/photo-1721332154161-847851ea188b?q=80&w=3435&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        },
+    ];
 }
